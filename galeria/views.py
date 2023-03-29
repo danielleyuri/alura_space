541d13383d7695ffc060b3751d7e5eb3404b8a94
Danielle <danielleyuri-jp>
2023-03-29 12:46:25 -0300

    adicionado dois campos no model Fotografia de data e um campo booleano

--- galeria/views.py
+++ galeria/views.py
@@ -4,8 +4,10 @@ from galeria.models import Fotografia
 
 
 def index(request):
-    fotografias = Fotografia.objects.all()
-    return render(request, 'galeria/index.html', {"cards": fotografias})
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada= True)
+
+    return render(request,'galeria/index.html', {"cards": fotografias})
+
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
