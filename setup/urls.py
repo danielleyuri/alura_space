2fd5ed232b04a759309119ad0023947cf6f83258
Danielle <danielleyuri-jp>
2023-04-25 15:09:34 -0300

    Conclusão do projeto

--- setup/urls.py
+++ setup/urls.py
@@ -4,8 +4,16 @@ from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
 
+# código omitido
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('galeria.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-                                                         
+        path('admin/', admin.site.urls),
+        path('', include('apps.galeria.urls')),
+        path('', include('apps.usuarios.urls')),
+]     
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
